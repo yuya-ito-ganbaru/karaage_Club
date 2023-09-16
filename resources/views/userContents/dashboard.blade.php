82ea20cf01a6963355ee53f351bedd24aab854ed
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-09-16 22:55:04 +0900

    管理画面として使用予定　今の所メモ帳

--- resources/views/userContents/dashboard.blade.php
+++ resources/views/userContents/dashboard.blade.php
@@ -10,6 +10,17 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <br>
+                    <h1>やること</h1>
+                    <ul>
+                        <li>バリデーション処理</li>
+                        <li>エラーメッセージの処理</li>
+                        <li>クリック実行確認メッセージ処理</li>
+                        <li>保存したimageファイルの削除処理（編集や入力内容変更時）</li>
+                        <li>レスポンシブ対応</li>
+                        <li>styleのリファクタリング対応</li>
+                        <li>jsの読み込みタイミングの確認、表示部分の読み込みでエラーが出ている。※idが見つからないエラーなので読み込みタイミングの調整で解決できると思う。</li>
+                    </ul>
                 </div>
             </div>
         </div>
