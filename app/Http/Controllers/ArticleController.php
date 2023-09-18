4d551798c661eac0038a54de63821c68a4a16789
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2023-09-18 18:06:00 +0900

    投稿、編集、削除機能の作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -18,6 +18,7 @@ class ArticleController extends Controller
     {
         //ログインユーザー確認
         $user = Auth::user();
+
         return view('/userContents/articlePosts/articlePost', compact('user'));
     }
     /**
@@ -26,21 +27,22 @@ class ArticleController extends Controller
     public function postCreate(Request $request)
     {
         //dd($_POST);
+        //ログインユーザー確認
+        $user = Auth::user();
+        //プロフィールモデルのインスタンス作成
+        $article = new Article();
+
         //ディレクトリ名
         $dir = 'article_images';
         if ($request->hasFile('image')) {
             //ファイル名を取得
-            $file_name = $request->file('image')->getClientOriginalName();
+            //$file_name = $request->file('image')->getClientOriginalName();
+            $file_name = $user->id . '_' . date('Y_m_d_His') . '_' . $request->file('image')->getClientOriginalName();
             //storage/public/imgに画像を保存
             $request->file('image')->storeAs('public/' . $dir, $file_name);
         } else {
             $file_name = null;
         }
-
-        //ログインユーザー確認
-        $user = Auth::user();
-        //プロフィールモデルのインスタンス作成
-        $article = new Article();
 
         $article->user_id = $user->id;
         $article->title = $request->input('title');
@@ -48,7 +50,7 @@ class ArticleController extends Controller
         $article->image = $file_name;
         $article->body = $request->input('body');
         $article->recommend = $request->input('recommend');
-        
+
         return view('/userContents/articlePosts/articleConfirm', compact('article'));
     }
     /**
@@ -75,11 +77,84 @@ class ArticleController extends Controller
     /**
      * 投稿記事検索機能
      */
-    public function articleSearch() {
+    public function articleSearch()
+    {
         //ログインユーザー確認
         $user = Auth::user();
         $articleList = Article::where('user_id', $user->id)->get();
         return view('/userContents/articleLists/articleList', compact('articleList', 'user'));
     }
+    /**
+     * 投稿記事表示機能
+     */
+    public function articleView($id)
+    {
+        $articles = Article::where('id', $id)->get();
+        return view('/userContents/articleLists/articleView', compact('articles'));
+    }
+    /**
+     * 投稿記事削除機能
+     */
+    public function articleDelete(Request $request)
+    {
+        //dd($_POST);
+        $article = Article::find($request->input('id'));
+        //dd($article);
+        // 既存画像ファイル削除
+        if ($article->image) {
+            $dir = 'article_images';
+            $imagePath = 'public/' . $dir . '/' . $article->image;
 
+            // 画像ファイルを削除
+            if (Storage::exists($imagePath)) {
+                Storage::delete($imagePath);
+            }
+        }
+        $article->delete();
+        return redirect()->route('articleList');
+    }
+
+    /**
+     * 編集画面表示
+     */
+    public function articleEditView($id)
+    {
+        $articles = Article::where('id', $id)->get();
+        return view('/userContents/articleLists/articleEdit', compact('articles'));
+    }
+    /**
+     * 投稿記事編集機能
+     */
+    public function articleUpData(Request $request)
+    {
+        //dd($_POST);
+        $user = Auth::user();
+        $article = Article::find($request->input('id'));
+        $dir = 'article_images';
+
+        if ($request->hasFile('image')) {
+            $imagePath = 'public/' . $dir . '/' . $article->image;
+
+            // 画像ファイルを削除
+            if (Storage::exists($imagePath)) {
+                Storage::delete($imagePath);
+            }
+            //ファイル名を取得
+            //$file_name = $request->file('image')->getClientOriginalName();
+            $file_name = $user->id . '_' . date('Y_m_d_His') . '_' . $request->file('image')->getClientOriginalName();
+            //storage/public/imgに画像を保存
+            $request->file('image')->storeAs('public/' . $dir, $file_name);
+            $article->image = $file_name;
+        }
+
+        $article->user_id = $user->id;
+        $article->title = $request->input('title');
+        $article->tag = $request->input('tag');
+
+        $article->body = $request->input('body');
+        $article->recommend = $request->input('recommend');
+
+        $article->save();
+        return redirect()->route('articleList');
+    }
 }
